f4dc2949d0ce12ecc552407758931c2f65aa4bba
wan1151 <whafizz1151@gmail.com>
2025-12-27 01:07:42 +0800

    update db setting (public - laravel)

--- config/database.php
+++ config/database.php
@@ -74,17 +74,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),    
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST'),
-            'port' => env('DB_PORT', '5432'),    
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
-            'search_path' => env('DB_SCHEMA', 'laravel'),    
-            'username' => env('DB_USERNAME', 'root'),    
-            'password' => env('DB_PASSWORD', ''),    
-            'charset' => env('DB_CHARSET', 'utf8'),    
-            'prefix' => '',    
-            'prefix_indexes' => true,    
-            'search_path' => 'laravel',    
+            'search_path' => env('DB_SCHEMA', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+
             'sslmode' => 'prefer',
         ],
 
