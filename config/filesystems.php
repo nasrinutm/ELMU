726eca58ffb7d81f068bc6ef6d25c33728c5b750
wan1151 <whafizz1151@gmail.com>
2026-01-16 03:37:03 +0800

    "improve"

--- config/filesystems.php
+++ config/filesystems.php
@@ -6,9 +6,13 @@ return [
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
     |--------------------------------------------------------------------------
+    |
+    | FIX: We change the default to 'supabase'. This ensures that any code
+    | using Storage::put() without a disk name will automatically go to the cloud.
+    |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'supabase'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,9 +57,10 @@ return [
             'key'    => env('SUPABASE_STORAGE_ACCESS_KEY_ID'),
             'secret' => env('SUPABASE_STORAGE_SECRET_ACCESS_KEY'),
             'bucket' => env('SUPABASE_STORAGE_BUCKET'),
-            'region' => env('SUPABASE_STORAGE_REGION'),
+            'region' => env('SUPABASE_STORAGE_REGION', 'us-east-1'), // Added default region
             'endpoint' => env('SUPABASE_STORAGE_ENDPOINT'),
             'use_path_style_endpoint' => true,
+            'visibility' => 'public', // Added visibility
             'throw'  => true,
         ],
 
